7a8bda84ea2dcf7b6059fe1675fa0928c3e6ee24
Tom Wyatt <tom@span-eng.com>
2025-03-06 11:50:20 +1000

    change where camera presets come from

--- camera_ui/remote-component-hls/src/RemoteComponent.js
+++ camera_ui/remote-component-hls/src/RemoteComponent.js
@@ -7,17 +7,24 @@ export default class RemoteComponent extends RemoteAccess {
     const agent_id = this.getUi().agent_key;
     const ui = this.getUiState();
     const reported = ui.reported;
+
+    const cam_name = reported.cam_name || reported.name
+
+    const cam_settings = this.getParam(cam_name)
+    const presets = cam_settings?.presets || []
+    const activePreset = cam_settings?.active_preset
+
     // console.log(reported);
     return <CameraLiveView
       agentId={agent_id}
-      camName={reported.cam_name || reported.name}
+      camName={cam_name}
       camType={reported.cam_type}
       camHostname={reported.address}
       camManagePort={reported.managePort || 80}
       rtspServerHost={reported?.rtspServerHost ?? "localhost"}
       rtspServerPort={reported?.rtspServerPort ?? 8083}
-      camPresets={reported?.presets || []}
-      activePreset={reported.active_preset}
+      camPresets={presets}
+      activePreset={activePreset}
     />
   }
 }
